7a40fcae4967db55ae22e5a8d9292edfef7de7a8
ansarimofid <ansarimofid@gmail.com>
2017-08-01 16:16:58 +0530

    Refactor: Optimizes code; Docs: Adds doc

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,15 +1,23 @@
 import media
 
 # Movie 1 Data
-toy_story = media.Movie('Toy Story', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum diam dui, volutpat id iaculis sit amet, semper at nibh.', 'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg', 'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+toy_story = media.Movie('Toy Story',
+                        'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+                        'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 # Movie 2 Data
-avatar = media.Movie('Avatar', "Avatar, marketed as James Cameron's Avatar, is a 2009 American[7][8]", 'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg', 'https://www.youtube.com/watch?v=aVdO-cx-McA')
+avatar = media.Movie('Avatar',
+                      'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',
+                     'https://www.youtube.com/watch?v=aVdO-cx-McA')
 
 # Movie 3 Data
-despicable_me = media.Movie('Despicable Me 3', "Despicable Me 3[a] is a 2017 American 3D computer-animated comedy", 'https://upload.wikimedia.org/wikipedia/en/9/91/Despicable_Me_3_%282017%29_Teaser_Poster.jpg', 'https://www.youtube.com/watch?v=euz-KBBfAAo')
+despicable_me = media.Movie('Despicable Me 3',
+                            'https://upload.wikimedia.org/wikipedia/en/9/91/Despicable_Me_3_%282017%29_Teaser_Poster.jpg',
+                            'https://www.youtube.com/watch?v=euz-KBBfAAo')
 
 # Movie 4 Data
-inception = media.Movie('Inception', "Inception is a 2010 science fiction film written, co-produced, and directed by Christopher Nolan, and co-produced by Emma Thomas. The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious", 'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg', 'https://www.youtube.com/watch?v=YoHD9XEInc0')
+inception = media.Movie('Inception',
+                        'https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg',
+                        'https://www.youtube.com/watch?v=YoHD9XEInc0')
 
 
 # Movie List
--- media.py
+++ media.py
@@ -1,13 +1,15 @@
-import webbrowser
-
-# Creates Movie instance from supplied parameters
 class Movie:
-    def __init__(self,movie_title,movie_storyline,movie_poster_image_url,movie_trailer_youtube_url):
-        self.title  = movie_title
-        self.storyline = movie_storyline
+    """
+    Movie Class
+    """
+    def __init__(self, movie_title, movie_poster_image_url, movie_trailer_youtube_url):
+        """It called when object is created
+
+        Args:
+            movie_title(str): Title of movie
+            movie_poster_image_url(str): Url of the poster
+            movie_trailer_youtube_url(str): Url of the movie trailer
+        """
+        self.title = movie_title
         self.poster_image_url = movie_poster_image_url
         self.trailer_youtube_url = movie_trailer_youtube_url
-
-    # Opens trailer for the given movie
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
